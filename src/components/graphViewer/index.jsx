1a997f5e7fea0e9e5376ed6136b2cbb7fe66b3b6
Rafael Angel Becerra Blasco <rbecerra@belatrix.com>
2019-07-30 13:26:02 -0500

    feat(graphViewer): add gltf export feature

--- src/components/graphViewer/index.jsx
+++ src/components/graphViewer/index.jsx
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { ForceGraph3D } from 'react-force-graph';
 import SpriteText from 'three-spritetext';
+import * as THREE from 'three';
+import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter';
+import { saveAs } from 'file-saver';
+
 import { GraphViewContainer } from './styles';
 
+function saveArrayBuffer(buffer, filename) {
+  saveAs(new Blob([buffer], { type: 'application/octet-stream' }), filename);
+}
+
+function saveString(text, filename) {
+  saveAs(new Blob([text], { type: 'text/plain' }), filename);
+}
+
+const exportModel = (graphViewer) => {
+  const exporter = new GLTFExporter();
+  exporter.parse(graphViewer.state.comp.scene(), (result) => {
+    if (result instanceof ArrayBuffer) {
+      saveArrayBuffer(result, 'scene.glb');
+    } else {
+      const output = JSON.stringify(result, null, 2);
+      console.log(output);
+      saveString(output, 'scene.gltf');
+    }
+  });
+};
+
 const GraphViewer = ({ graphData, config }) => {
-  const fg = React.createRef();
+  let fg;
   const handleClick = (node) => {
     const distance = 40;
     const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
@@ -16,10 +41,14 @@ const GraphViewer = ({ graphData, config }) => {
   };
 
   return (
-    <GraphViewContainer>
-      {graphData && (
+    <Fragment>
+      <GraphViewContainer>
+        {graphData && (
         <ForceGraph3D
-          ref={fg}
+          ref={(el) => {
+            fg = el;
+            console.log(el);
+          }}
           width={window.innerWidth - 410}
           graphData={graphData}
           nodeLabel="label"
@@ -43,8 +72,19 @@ const GraphViewer = ({ graphData, config }) => {
             }
           }}
         />
-      )}
-    </GraphViewContainer>
+        )}
+      </GraphViewContainer>
+
+      <button onClick={() => {
+        console.log(fg);
+        exportModel(fg);
+      }}
+      >
+Exportar
+
+      </button>
+
+    </Fragment>
   );
 };
 
